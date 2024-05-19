09b70354504465c0fdcad27d8882d189cd98385a
nehru00123@gmail.com <nehru00123@gmail.com>
2024-05-19 06:56:24 +0530

    diagonal difference is added

--- src/diagonalDifference.java
+++ src/diagonalDifference.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class diagonalDifference {
+import java.util.List;
+
+public class diagonalDifference {
+public static int diagDiff(List<List<Integer>> arr){
+    int n = arr.size();
+    int leftDiagonalSum = 0;
+    int rightDiagonalSum = 0;
+    for (int i=0; i<n; i++){
+        List<Integer> row = arr.get(i);
+        leftDiagonalSum += row.get(i); //Sum of elements in left diagonal
+        rightDiagonalSum += row.get(n-1-i); //Sum of elements in the right diagonal
+    }
+    return Math.abs(leftDiagonalSum - rightDiagonalSum);
+}
+public static void main(String[] args){
+    List<List<Integer>> matrix = List.of(
+            List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9));
+    int absoluteDifference = diagDiff(matrix);
+    System.out.println("Absolute Difference: " + absoluteDifference);
+}
 }
